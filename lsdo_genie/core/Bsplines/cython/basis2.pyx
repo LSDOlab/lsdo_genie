d303d506b1738dff44eb7b189a47b2e4afdbf388
Ryan <rcdunn@ucsd.edu>
2023-04-03 12:40:32 -0700

    fixing cython build

--- lsdo_genie/core/Bsplines/cython/basis2.pyx
+++ lsdo_genie/core/Bsplines/cython/basis2.pyx
@@ -1,3 +1,5 @@
+from libc.stdlib cimport malloc, free
+
 cdef int get_basis2(int order, int num_control_points, double u, double* knot_vector, double* basis2):
     cdef int i, j1, j2, l, n
     cdef double den, b0_a, b0_b, b1_a, b1_b, b2_a, b2_b
