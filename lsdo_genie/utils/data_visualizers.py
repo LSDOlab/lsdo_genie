e4ad0fdaac23c3f2ecac5abd00be35c3044f469e
Ryan <rcdunn@ucsd.edu>
2023-04-19 23:16:24 -0700

    improved normal visualization, vertex points nomenclature

--- lsdo_genie/utils/data_visualizers.py
+++ lsdo_genie/utils/data_visualizers.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def visualize_2Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool=True, title:str="2D Point cloud with normals"):
+def visualize_2Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool=True, title:str="2D Point cloud with normals", perc_normal_length:float=5.):
     '''
     Plots and shows a 2D point cloud with normal vectors
 
@@ -17,6 +17,11 @@ def visualize_2Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool
     title : str
         Title of the plot
     '''
+    lower = np.min(points,axis=0)
+    upper = np.max(points,axis=0)
+    domain = np.stack((lower, upper),axis=1)
+    bbox_diag = np.linalg.norm(np.diff(domain,axis=1).flatten())
+    normal_length = perc_normal_length/100*bbox_diag
     sns.set_style('ticks')
     plt.figure()
     plt.plot(points[:,0],points[:,1],'k.', label="points")
@@ -24,17 +29,17 @@ def visualize_2Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool
         show_label = True
         for (x,y),(nx,ny) in zip(points, normals):
             if show_label:
-                plt.arrow(x, y, nx, ny, color='b', head_width=.2, label="normals")
+                plt.arrow(x, y, nx*normal_length, ny*normal_length, color='b', head_width=.2, label="normals")
                 show_label = False
             else: 
-                plt.arrow(x, y, nx, ny, color='b', head_width=.2)
+                plt.arrow(x, y, nx*normal_length, ny*normal_length, color='b', head_width=.2)
     plt.title(title)
     plt.legend()
     plt.axis('equal')
     sns.despine()
     plt.show()
 
-def visualize_3Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool=True, title:str="3D Point cloud with normals"):
+def visualize_3Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool=True, title:str="3D Point cloud with normals", perc_normal_length:float=5.):
     '''
     Plots and shows a 3D point cloud with normal vectors
 
@@ -49,6 +54,11 @@ def visualize_3Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool
     title : str
         Title of the plot
     '''
+    lower = np.min(points,axis=0)
+    upper = np.max(points,axis=0)
+    domain = np.stack((lower, upper),axis=1)
+    bbox_diag = np.linalg.norm(np.diff(domain,axis=1).flatten())
+    normal_length = perc_normal_length/100*bbox_diag
     sns.set_style('ticks')
     plt.figure()
     ax = plt.axes(projection='3d')
@@ -57,15 +67,15 @@ def visualize_3Dptcloud(points:np.ndarray, normals:np.ndarray, show_normals:bool
         show_label = True
         for (x,y,z),(nx,ny,nz) in zip(points, normals):
             if show_label:
-                xpts = np.array([x,x+nx])
-                ypts = np.array([y,y+ny])
-                zpts = np.array([z,z+nz])
+                xpts = np.array([x,x+nx*normal_length])
+                ypts = np.array([y,y+ny*normal_length])
+                zpts = np.array([z,z+nz*normal_length])
                 ax.plot(xpts,ypts,zpts, 'b.-', label="normals")
                 show_label = False
             else: 
-                xpts = np.array([x,x+nx])
-                ypts = np.array([y,y+ny])
-                zpts = np.array([z,z+nz])
+                xpts = np.array([x,x+nx*normal_length])
+                ypts = np.array([y,y+ny*normal_length])
+                zpts = np.array([z,z+nz*normal_length])
                 ax.plot(xpts,ypts,zpts, 'b-')
     ax.set_title(title)
     ax.legend()
--- lsdo_genie/utils/normal_approx.py
+++ lsdo_genie/utils/normal_approx.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def vertex_normal_approx(surf_pts):
+def vertex_normal_approx(vertices:np.ndarray):
     '''
     A quick way to approximate the normal vectors from an arbitrary closed polygon.
     Normals are an average between the 2 edge normals adjacent to a point and follow a covention:
@@ -17,15 +17,15 @@ def vertex_normal_approx(surf_pts):
     normals : np.ndarray(num_pts,2)
         Approximate normal vectors at the input points
     '''
-    edges_i = surf_pts - np.roll(surf_pts, shift=1, axis=0)
+    edges_i = vertices - np.roll(vertices, shift=1, axis=0)
     normals_i = np.stack((-edges_i[:,1],edges_i[:,0]),axis=1) / np.linalg.norm(edges_i,axis=1)[:,None]
-    edges_j = np.roll(surf_pts, shift=-1, axis=0) - surf_pts
+    edges_j = np.roll(vertices, shift=-1, axis=0) - vertices
     normals_j = np.stack((-edges_j[:,1],edges_j[:,0]),axis=1) / np.linalg.norm(edges_j,axis=1)[:,None]
     normals = (normals_i + normals_j)/2
     normals = normals/np.linalg.norm(normals,axis=1)[:,None]
     return normals
 
-def midpoint_normal_approx(vertices):
+def midpoint_normal_approx(vertices:np.ndarray):
     '''
     Approximate an arbitrary closed polygon by using the midpoints of the edges formed by an input set of vertices
     Normal direction is determined by the direction of the points given in the input:
